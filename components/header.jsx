1f1b984dd3ea7be766696942b085007b44087843
Cauã <cauadeassis2009@gmail.com>
2025-11-08 18:43:13 +0000

    abstracting `header` to a component

---
+++ components/header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from "../styles/header.module.css";
+export default function Header() {
+  return (
+    <header>
+      <h1>Vida Eletrônica</h1>
+    </header>
+  );
+}
