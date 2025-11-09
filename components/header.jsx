3ea9c350efdd8e70b01d83368c3f3b409224f7c5
Cauã <cauadeassis2009@gmail.com>
2025-11-09 19:22:22 +0000

    New `header` feature and style`

--- components/header.jsx
+++ components/header.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { useRouter } from "next/router";
 import styles from "../styles/header.module.css";
+
 export default function Header() {
+  const router = useRouter();
+  const handleTitleClick = () => {
+    router.push("/");
+  };
   return (
-    <header>
-      <h1>Vida Eletrônica</h1>
+    <header className={styles.header}>
+      <h1 onClick={handleTitleClick} className={styles.title}>
+        Vida Eletrônica
+      </h1>
     </header>
   );
 }
