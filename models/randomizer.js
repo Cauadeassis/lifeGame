0761a2775338b1184382db2aec0924b162baa7db
Cauã <cauadeassis2009@gmail.com>
2025-11-15 09:41:33 +0000

    fix bug

--- models/randomizer.js
+++ models/randomizer.js
@@ -16,17 +16,58 @@ export function generateRandomStatus() {
   }
   const ultimoValor = 240 - sumOffAll;
   allStatus[3] = ultimoValor;
-  for (
-    let currentPosition = allStatus.length - 1;
-    currentPosition > 0;
-    currentPosition--
-  ) {
+  for (let currentPosition = allStatus.length - 1; currentPosition > 0; currentPosition--) {
     const randomPosition = Math.floor(Math.random() * (currentPosition + 1));
-    [allStatus[currentPosition], allStatus[randomPosition]] = [
-      allStatus[randomPosition],
-      allStatus[currentPosition],
-    ];
+    [allStatus[currentPosition], allStatus[randomPosition]] = [allStatus[randomPosition], allStatus[currentPosition]];
   }
   console.log(allStatus);
   return allStatus;
 }
+export function generateRandomCharacter() {
+  const countries = Object.keys(namesByCountry);
+  const getRandomItem = (array) => array[Math.floor(Math.random() * array.length)];
+  const genders = [
+    { id: "male", label: "Masculino" },
+    { id: "female", label: "Feminino" }
+  ];
+  const skinTones = {
+    male: [
+      { id: "white", label: "Branco" },
+      { id: "brown", label: "Moreno" },
+      { id: "black", label: "Negro" },
+    ],
+    female: [
+      { id: "white", label: "Branca" },
+      { id: "brown", label: "Morena" },
+      { id: "black", label: "Negra" },
+    ],
+  };
+
+  const incomes = {
+    male: [
+      { id: "poor", label: "Pobre" },
+      { id: "middle", label: "Classe Média" },
+      { id: "rich", label: "Rico" }
+    ],
+    female: [
+      { id: "poor", label: "Pobre" },
+      { id: "middle", label: "Classe Média" },
+      { id: "rich", label: "Rica" }
+    ]
+  };
+  const gender = getRandomItem(genders);
+  const country = getRandomItem(countries);
+  const firstName = getRandomItem(namesByCountry[country][gender.id]);
+  const lastName = getRandomItem(namesByCountry[country]["last"]);
+  const skinTone = getRandomItem(skinTones[gender.id]);
+  const income = getRandomItem(incomes[gender.id]);
+  return {
+    firstName,
+    lastName,
+    country,
+    gender,
+    skinTone,
+    income
+  };
+}
+
