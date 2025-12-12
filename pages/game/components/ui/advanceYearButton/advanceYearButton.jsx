92c9ef979ccbfa018006979b675d29ee493d7416
Cauã <cauadeassis2009@gmail.com>
2025-12-12 21:42:26 +0000

    New structure

---
+++ pages/game/components/ui/advanceYearButton/advanceYearButton.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./actionButton.module.css";
+
+const AdvanceYearButton = ({ onClick, disabled = false }) => {
+  return (
+    <button
+      className={`${styles.button} ${styles.advance}`}
+      onClick={onClick}
+      disabled={disabled}
+      aria-label="Avançar para o próximo ano"
+    >
+      <span>+</span>
+    </button>
+  );
+};
+
+export default AdvanceYearButton;
